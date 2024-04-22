3acec2f5f6b74acce549c02da7cdf87de3f55a58
bluuweb <contacto@bluuweb.cl>
2024-04-22 10:56:41 -0400

    base

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { Route, Routes } from "react-router-dom";
+
+import HomePage from "./pages/Home";
+import LoginPage from "./pages/Login";
+import RegisterPage from "./pages/Register";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={<HomePage />}
+      />
+      <Route
+        path="/login"
+        element={<LoginPage />}
+      />
+      <Route
+        path="/register"
+        element={<RegisterPage />}
+      />
+    </Routes>
+  );
+};
+export default App;
